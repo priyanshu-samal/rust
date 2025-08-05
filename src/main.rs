de814bc650f63a4489e399cc3b7151b9873387ca
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-08-05 09:22:19 +0530

    yw

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,8 @@
-fn main() {
-    let x: i32 = 1;
-    print!("x is : {} ", x);
-    let is_male=true;
-    print!("{} ",is_male); 
-    let greeting: String =String::from("hello world");
-    println!("{} ", greeting);
+fn main(){
+    let v1= vec![1, 2, 3];
+    let it=v1.iter();
 
-    let a = true;
-    if a {
-        print!("ok");
-    } else {
-        print!("not ok");
-    }
-
-    for i in 0..100 {
-        print!("{} ", i);
-    }
-   
-   let f = 10;
-   let g = 20;
-   let sum = do_sum(f, g);
-   print!("sum is : {} ", sum);
-
-
-    fn do_sum(f: i32, g: i32) -> i32 {
-        return f + g;
-    }
-    
-}
- 
+    for i in it {
+        println!("{}", i);
+    } 
+} 
